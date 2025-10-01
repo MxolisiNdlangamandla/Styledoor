6259b435cbc29de826f48a887283a0f576d37b01
Mxolisi Ndlangamandla <mxolisi.ndlangamandla@weconnect.tech>
2025-10-01 10:20:09 +0200

    adjusted the logo size, changed the display for landing buttons on the lading to display row instead of column,and adjusted the text to justify evenly

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import logo from "../assets/Logo.png";
 function Header() {
   return (
     <header className="brand-header">
-      <img src={logo} alt="WAASHA Logo" className="brand-logo" />
+      <img src={logo} alt="WAASHA Logo" className="brand-logo" width={150} height={150}/>
       <h1 className="brand-title">WAASHA</h1>
       <div className="brand-slogan">The Future Of Service, Today</div>
     </header>
--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-
 function SignupForm({ role = "Client" }) {
   const navigate = useNavigate();
   const [form, setForm] = useState({
